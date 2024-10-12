dc283c2fe46abe784cea04ba2a66188803733693
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-10-12 15:44:26 +0300

    feat: implement basic functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('test');
+import { createInterface } from 'readline';
+// import os from 'os';
+import path from 'path';
+import { homedir } from 'os';
+import { chdir, cwd } from 'process';
+
+const username = process.argv.find((arg) => arg.startsWith('--username=')).split('=')[1] || 'User';
+console.log(`Welcome to the File Manager, ${username}!`);
+
+let currentDir = homedir();
+chdir(currentDir);
+printCurrentDirectory();
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  // prompt: 'Enter command: ',
+});
+
+// rl.prompt();
+
+rl.on('line', (line) => {
+  if (line === '.exit') {
+    rl.close();
+  }
+});
+
+rl.on('close', () => {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+});
+
+function printCurrentDirectory() {
+  console.log(`You are currently in ${currentDir}`);
+}
